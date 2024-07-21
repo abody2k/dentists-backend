c8bf40eafd39a891fe43e46d2c84611f2f29fbcd
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-07-21 10:10:48 +0300

    toc

---
+++ build/client/_app/immutable/entry/start.Bph4_MGL.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.Dwr23cDU.js";export{t as start};
--- build/server/chunks/1-B51feerm.js
+++
@@ -1,9 +0,0 @@
-const index = 1;
-let component_cache;
-const component = async () => component_cache ??= (await import('./error.svelte-kQzXVS9D.js')).default;
-const imports = ["_app/immutable/nodes/1.D_6ok0so.js","_app/immutable/chunks/scheduler.C7Xo_3aa.js","_app/immutable/chunks/index.NYh16Rov.js","_app/immutable/chunks/entry.LgM9fU2C.js","_app/immutable/chunks/index.DTVtpEo5.js"];
-const stylesheets = [];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=1-B51feerm.js.map
