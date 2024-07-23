5cac0fa1b2a3fe8cf05ed77f0e9c882157f1b4ec
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-07-23 11:44:12 +0300

    lok

--- build/server/chunks/16-BTP8huec.js
+++
@@ -1,9 +0,0 @@
-const index = 16;
-let component_cache;
-const component = async () => component_cache ??= (await import('./_page.svelte-Bimb6qQK.js')).default;
-const imports = ["_app/immutable/nodes/16.BhCG_oFl.js","_app/immutable/chunks/scheduler.C7Xo_3aa.js","_app/immutable/chunks/index.NYh16Rov.js","_app/immutable/chunks/Bar.BCgXY4Hk.js","_app/immutable/chunks/each.DGNUWULW.js","_app/immutable/chunks/not.CMcaOXzY.js","_app/immutable/chunks/preload-helper.D6kgxu3v.js"];
-const stylesheets = ["_app/immutable/assets/16.HI_oOEgL.css","_app/immutable/assets/Bar.BNHf8-ew.css","_app/immutable/assets/Button.BuICBtmV.css","_app/immutable/assets/NotificationBubble.DM2sZayM.css"];
-const fonts = [];
-
-export { component, fonts, imports, index, stylesheets };
-//# sourceMappingURL=16-BTP8huec.js.map
