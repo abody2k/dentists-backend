093ae16b7098aa3ce7f72e00038f2acd0a2877a6
Abdulrahman <Abdulrahman.7.2k@gmail.com>
2024-07-23 13:25:14 +0300

    kkk

---
+++ build/client/_app/immutable/entry/start.CqHzFIXd.js
@@ -0,0 +1,1 @@
+import{a as t}from"../chunks/entry.7jvu7s5E.js";export{t as start};
---
+++ build/server/chunks/_page.svelte-Cf4xUftT.js
@@ -0,0 +1,12 @@
+import { c as create_ssr_component, v as validate_component } from './ssr-JWmjUSZd.js';
+import { E as Exam } from './Exam-vRZ3bT8x.js';
+import './Bar-DSmYRrjO.js';
+
+const Page = create_ssr_component(($$result, $$props, $$bindings, slots) => {
+  let { data } = $$props;
+  if ($$props.data === void 0 && $$bindings.data && data !== void 0) $$bindings.data(data);
+  return `${validate_component(Exam, "Exam").$$render($$result, { data }, {}, {})}`;
+});
+
+export { Page as default };
+//# sourceMappingURL=_page.svelte-Cf4xUftT.js.map
